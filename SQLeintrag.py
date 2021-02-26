f1d40c44dcca2c30fa131618d56660bee04ce4e5
MoeMitDemO <45233085+MoeMitDemO@users.noreply.github.com>
2021-02-26 08:41:07 +0100

    Connect und Create

---
+++ SQLeintrag.py
@@ -0,0 +1,32 @@
+import sqlite3, mysql.connector
+from sqlite3 import Error
+from mysql.connector import Error
+
+
+def create_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password
+        )
+        print("Connection to MySQL DB successful")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+    return connection
+
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as e:
+        print(f"The error '{e}' occurred")
+
+connection = create_connection("localhost", "root", "")
+
+create_database_query = "CREATE DATABASE sm_app"
+create_database(connection, create_database_query)
+

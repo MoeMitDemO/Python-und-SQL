a113fb801583e0828678671bfa66739423353dd0
MoeMitDemO <45233085+MoeMitDemO@users.noreply.github.com>
2021-03-26 08:50:13 +0100

    grwet

--- SQLeintrag.py
+++ SQLeintrag.py
@@ -1,7 +1,14 @@
+from os import P_WAIT
 import sqlite3, mysql.connector
 from sqlite3 import Error
 from mysql.connector import Error
+from io import open
 
+
+host = input("Hostadresse: ")
+user = input("Benutzer: ")
+pw = input("Passwort: ")
+db = input("Datenbank: ")
 
 def create_connection(host_name, user_name, user_password, db_name):
     connection = None
@@ -12,7 +19,7 @@ def create_connection(host_name, user_name, user_password, db_name):
             passwd=user_password,
             database=db_name
         )
-        print("Connection to MySQL DB successful")
+        print("Verbindung zur MEINEFORTSETZUNG DATENBANK erfolgreich aufgebaut")
     except Error as e:
         print(f"The error '{e}' occurred")
 
@@ -26,6 +33,19 @@ def create_database(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-connection = create_connection("localhost", "root", "", "python-test")
+#"localhost", "root", "", "python-test"
+connection = create_connection(host, user, pw, db)
 
+mycursor = connection.cursor()
 
+ziel = input("Name der auszugebenden Tabelle:")
+executeString = "SELECT * FROM " + ziel
+mycursor.execute(executeString)
+ausgabe = ""
+for x in mycursor:
+    ausgabe += str(x) + "\n"
+
+print(ausgabe)
+
+f = open("log.txt", mode="w", encoding="utf-8")
+f.write(ausgabe)

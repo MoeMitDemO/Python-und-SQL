d638538a9e4b72256f9ceedf487b2f6fe38ab540
MoeMitDemO <45233085+MoeMitDemO@users.noreply.github.com>
2021-04-23 09:50:05 +0200

    asd

--- tabula.py
+++ tabula.py
@@ -6,7 +6,7 @@ import sys,win32api
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()                  # Rufe die Klasse auf
-        uic.loadUi('you.ui', self)        # Lade die UI Datei
+        uic.loadUi('yo.ui', self)        # Lade die UI Datei
         self.show()             
 
 
@@ -18,33 +18,10 @@ def selectSource():
     if fileName:
         print(fileName)
         window.quelleLineEdit.setText(fileName)
-        
-def selectDestination():
-    options = QFileDialog.Options()
-    options |= QFileDialog.DontUseNativeDialog
-    fileName, _ = QFileDialog.getSaveFileName(window,"QFileDialog.getSaveFileName()","","All Files (*);;PDF (*.pdf)", options=options)
-    if fileName:
-        print(fileName)
-        window.zielLineEdit.setText(fileName)
 
 def convert():
-    input_file =  window.quelleLineEdit.text()
-    #give your file name with valid path 
-    # output_file = window.zielLineEdit.text()
-    output_file = r'C:\Users\Moe\Desktop\test.pdf'
-    #give valid output file name and path
-    app1 = client.DispatchEx("Excel.Application")
-    app1.Interactive = False
-    app1.Visible = False
-    Workbook = app1.Workbooks.Open(input_file)
-    try:
-        Workbook.ActiveSheet.ExportAsFixedFormat(0, output_file)
-    except Exception as e:
-        print("Failed to convert in PDF format.Please confirm environment meets all the requirements  and try again")
-        print(str(e))
-    finally:
-        Workbook.Close()
-        excel.app1.Exit()
+    print("convert")
+    
 
 app = QtWidgets.QApplication(sys.argv)              # Erzeuge eine Instanz von QtWidgets.QApplication
 window = Ui()                                       # Erzeuge eine Instanz unserer Klasse (Ui)
@@ -53,10 +30,3 @@ window.quelleButton.clicked.connect(selectSource)      # Legt fest, dass bei Kno
 window.zielButton.clicked.connect(selectDestination)        # Legt fest, dass bei Knopfdruck auf "Info" die Funktion "zeigeInfo" ausgeführt wird
 window.convertButton.clicked.connect(convert) 
 app.exec_()                                         # Starte die Anwendung
-
-
-
-
-
-
-
